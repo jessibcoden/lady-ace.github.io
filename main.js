2d9894b2903d219c7ad3ce61401255f1027803eb
Jess Brawner <jessnbillbrawner@gmail.com>
2017-09-02 10:42:51 -0500

    changed to es6

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var allBlogs = [];
+const allBlogs = [];
 
 
-var blog1 = {
+const blog1 = {
 	title: "Hip, hip, Array!",
 	subTitle: "Week 3",
 	date: "July 30, 2017",
@@ -14,7 +14,7 @@ var blog1 = {
 		"After suffering a brain sprain I was able to get my feet under me and even pulled off a nested loop in the Boy Bands project. I’m absolutely loving this… even the frustration is fun!"],
 }
 
-var blog2 = {
+const blog2 = {
 	title: "I’ll Never Eat Another Nugget",
 	subTitle: "Week 4",
 	date: "August 6, 2017",
@@ -30,21 +30,21 @@ var blog2 = {
 		],
 }
 
-var blog3 = {
+const blog3 = {
 	title: "Entry3",
 	subTitle: "Week 5",
 	date: "August 13, 2017",
 	content: ["Wow, this is the best free scrapbooking class I've ever taken! ps This one really cracks me up for some reason. No. I was ashamed to be _seen_ with you. I like being with you.You're losing blood, aren't you? Probably, my socks are wet. Hey, it was one night of wild passion! Michael: And yet you didn't notice her body? Gob: I like to look in the mirror.Maybe it was the eleven months he spent in the womb. The doctor said there were claw marks on the walls of her uterus. Stop licking my hand, you horse's ass! You mean the guy we're meeting with can't even grow his own hair? Come on!"],
 }
 
-var blog4 = {
+const blog4 = {
 	title: "Entry4",
 	subTitle: "Week 6",
 	date: "August 20, 2017",
 	content: ["Mr. Zuckerkorn, you've been warned about touching. You said spanking. Everything they do is so dramatic and flamboyant. It just makes me want to set myself on fire. - Lucille Bluth. No one was making fun of Andy Griffith. I can't emphasize that enough. Saw this on the highway and almost blue myself. Hop on? Obviously this blue part here is the land. Let me out that Queen. You boys know how to shovel coal?"],
 }
 
-var blog5 = {
+const blog5 = {
 	title: "Entry5",
 	subTitle: "Week 7",
 	date: "August 27, 2017",
@@ -57,13 +57,13 @@ allBlogs.push(blog3);
 allBlogs.push(blog4);
 allBlogs.push(blog5);
 
-var blogContainer = document.getElementById("blog-container");
+let blogContainer = document.getElementById("blog-container");
 
-for (var i = 0; i < allBlogs.length; i++){
+for (let i = 0; i < allBlogs.length; i++){
 
-	var currentBlog = allBlogs[i];
+	const currentBlog = allBlogs[i];
 
-	var domString = "";
+	const domString = "";
 	
 	domString += '<div class="blog">';
 	domString += 	'<header class="title">';
@@ -73,9 +73,9 @@ for (var i = 0; i < allBlogs.length; i++){
 	domString +=	'</header>';
 	domString +=	'<article class="content">'
 
-	for (var j = 0; j < currentBlog.content.length; j++) {
+	for (let j = 0; j < currentBlog.content.length; j++) {
 
-		var currentContent = currentBlog.content[j];
+		const currentContent = currentBlog.content[j];
 
 		domString += 		'<p>' + currentContent + '</p>';
 	}
